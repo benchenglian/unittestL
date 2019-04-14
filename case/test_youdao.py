06567826ca8881c80cb296ff0e397372a795b623
廉峰 <lianfeng@elensdata.com>
2019-04-14 15:47:43 +0000

    更新 test_youdao.py

--- case/test_youdao.py
+++ case/test_youdao.py
@@ -7,7 +7,7 @@ import unittest, time, re
 
 class Youdao(unittest.TestCase):
     def setUp(self):
-        self.driver = selenium.webdriver.remote.webdriver.WebDriver(command_executor="http://127.0.0.1:5001/wd/hub",
+        self.driver = selenium.webdriver.remote.webdriver.WebDriver(command_executor="http://192.168.1.3:5001/wd/hub",
                                                                     desired_capabilities=DesiredCapabilities.CHROME)
         self.base_url = "http://fanyi.youdao.com/?keyfrom=dict2.top"
         self.verificationErrors = []
