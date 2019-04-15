86f5d55cd10ce104ed6f9311ef1cb7deee4868a8
benchenglian <benchenglian@foxmai.com>
2019-04-15 23:45:19 +0800

    xx

---
+++ baidu.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+import selenium.webdriver.remote.webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+driver=selenium.webdriver.remote.webdriver.WebDriver(command_executor="127.0.0.1:4444//wd/hub",desired_capabilities=DesiredCapabilities.CHROME)          #获取浏览器的对象（此处的driver可以定义为任意字符，只是个对象）
+driver.get("http://www.baidu.com")
+driver.find_element_by_id("kw").send_keys("selenium")
+     #get方法发送网址
+driver.find_element_by_id("su").click()
+print(driver.title)
+driver.quit()
