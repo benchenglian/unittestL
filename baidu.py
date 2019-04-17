12136eeca93ea47779d4392a6b9a0e47eeca49fc
benchenglian <benchenglian@foxmai.com>
2019-04-18 00:03:47 +0800

    修改配置文件

--- baidu.py
+++ baidu.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import selenium.webdriver.remote.webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
+import urllib2
 
 driver=selenium.webdriver.remote.webdriver.WebDriver(command_executor="127.0.0.1:4444//wd/hub",desired_capabilities=DesiredCapabilities.CHROME)          #获取浏览器的对象（此处的driver可以定义为任意字符，只是个对象）
 driver.get("http://www.baidu.com")
